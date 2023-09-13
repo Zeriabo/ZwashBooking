48bc86d84dd8055b8b69f25afc8f790b9a405226
Zeriabo <zeriab@hotmail.com>
2023-09-13 22:43:06 +0300

    creating booking microservice

---
+++ src/main/java/com/zwash/booking/ZwashBookingApplication.java
@@ -0,0 +1,13 @@
+package com.zwash.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwashBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwashBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zwash/booking/ZwashBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwash.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwashBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
