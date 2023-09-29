8f52a04b822627170c24533bcbc173e96b1f3017
Zeriabo <zeriab@hotmail.com>
2023-09-29 22:33:55 +0300

    kafka producer

---
+++ src/main/java/com/zwash/booking/pojos/ProductRequest.java
@@ -0,0 +1,29 @@
+package com.zwash.booking.pojos;
+
+
+public class ProductRequest {
+    private String name;
+    private int price;
+	private String description; 
+	
+    public String getDescription() {
+ 		return description;
+ 	}
+ 	public void setDescription(String description) {
+ 		this.description = description;
+ 	}
+ 	public int getPrice() {
+ 		return price;
+ 	}
+ 	public void setPrice(int price) {
+ 		this.price = price;
+ 	}
+ 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
